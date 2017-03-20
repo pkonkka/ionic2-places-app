77c29a5d8b6445767ebd0e894f9914883b535a37
Petri Konkka <petrikonkka@gmail.com>
2017-03-20 13:16:31 +0200

    ongoing

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { PlacePage } from '../pages/place/place';
+import { AddPlacePage } from '../pages/add-place/add-place';
+import { SetLocationPage } from '../pages/set-location/set-location';
+
+import { AgmCoreModule } from 'angular2-google-maps/core';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    PlacePage,
+    AddPlacePage,
+    SetLocationPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBhtK_VUaNbKYKBzlx-rfnlAGYitykmEr4'
+    })
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    PlacePage,
+    AddPlacePage,
+    SetLocationPage    
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
---
+++ src/models/place.ts
@@ -0,0 +1,9 @@
+import { Location } from './location';
+
+export class Place {
+    constructor(
+        public title: string, 
+        public description: string,
+        public location: Location,
+        public imagePath: string) {}
+}
